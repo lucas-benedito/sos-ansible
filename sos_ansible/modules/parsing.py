bc1bf4617fe855c5825164f8bbd38131eee3df94
Jindrich Skacel <102800748+jskacel@users.noreply.github.com>
2023-08-10 16:54:25 +0200

    Adding parsing of case number from tarfile (#49)

--- sos_ansible/modules/parsing.py
+++ sos_ansible/modules/parsing.py
@@ -1,11 +1,16 @@
 """Custom parser to evaluate conditions"""
 import argparse
+import re
 
 
 class CheckDependsAction(argparse.Action):  # pylint: disable=too-few-public-methods
     """custom action to evaluate dependency on case number"""
 
     def __call__(self, parser, namespace, values, option_string=None):
+        casenum=re.search(r'-(\d{8})\-', values)
+        if casenum:
+            namespace.case = casenum.group().replace('-','')
+
         if namespace.case:
             namespace.tarball = [value.strip() for value in values.split(",")]
         else:
